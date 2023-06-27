37289a7a44e3f7dade858538c36957189fdee503
candela99 <gonzalezcandela759@gmail.com>
2023-06-26 21:32:36 -0300

    add popular movies

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const cargarPeliculas = async() => {
 	try {
-		const respuesta = await fetch('https://api.themoviedb.org/3/movie/1100?api_key=25df70143d25db476a828f0489a7a9ec&language=es-AR');
+		const respuesta = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=25df70143d25db476a828f0489a7a9ec&language=es-AR');
 		const datos = await respuesta.json();
+
+		let peliculas = '';
 		if(respuesta.status === 200) {
-			console.log(datos.title);
+			datos.results.forEach(pelicula => {
+				peliculas += `<h1>${pelicula.title}</h1>`;
+			});
+
+			document.getElementById('contenedor').innerHTML = peliculas;
 		} else if(respuesta.status === 401) {
 			console.log('Error: key no válida');
 		} else if(respuesta.status === 404) {
